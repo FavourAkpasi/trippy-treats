d1861dbea0bbe5efc77de0904cd252bed6c855d4
favourakpasi <akpasifavour@gmail.com>
2022-09-22 15:03:54 +0100

    footer links

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,17 +1,23 @@
 import React from "react";
 import "./Footer.css";
-import InstagramIcon from "@mui/icons-material/Instagram";
-import TwitterIcon from "@mui/icons-material/Twitter";
-import PhoneIcon from "@mui/icons-material/Phone";
-
+import { Instagram, Phone, Twitter, WhatsApp } from "@mui/icons-material";
 const Footer = () => {
   return (
     <footer>
       <img src="images/logo.png" alt="logo" />
       <div className="socials">
-        <InstagramIcon />
-        <TwitterIcon />
-        <PhoneIcon />
+        <a href="tel:0815 982 1641">
+          <Phone />
+        </a>
+        <a href="http://instagram.com/trippytreaats" target="_blank">
+          <Instagram />
+        </a>
+        <a href="http://twitter.com/trippytreatss" target="_blank">
+          <Twitter />
+        </a>
+        <a href="https://wa.me/%2B2348163429106" target="_blank">
+          <WhatsApp />
+        </a>
       </div>
     </footer>
   );
